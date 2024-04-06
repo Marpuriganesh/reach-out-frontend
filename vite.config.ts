eb17fb6f24c1e087bef1e27bad383bcb06d5c991
marpuri ganesh <marpuriganesh@gmail.com>
2024-04-06 14:12:33 +0530

    fix(PWA): IOS fix test

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,51 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import {VitePWA} from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
+  plugins: [
+    react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
       // devOptions: {
       //   enabled: true
       // },
-      includeAssets: ['reach_out_hand_logo.svg'],
+      includeAssets: ["reach_out_hand_logo.svg", "reach_out_hand_logo.png"],
       manifest: {
-        name: 'Reach Out',
-        short_name: 'Reach Out',
-        description: 'Reach Out is a platform for people to connect with each other.',
-        theme_color: '#33cfff',
-        display: 'standalone',
+        name: "Reach Out",
+        short_name: "Reach Out",
+        description:
+          "Reach Out is a platform for people to connect with each other.",
+        theme_color: "#33cfff",
+        background_color: "#33cfff",
+        display: "standalone",
         icons: [
           {
-            src: 'reach_out_hand_logo.svg',
-            sizes: 'any',
-            type: 'image/svg+xml',
-            purpose: 'any'
-          }
-        ]}
-    })
+            src: "reach_out_hand_logo.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+            purpose: "any",
+          },
+          {
+            src: "reach_out_hand_logo.png",
+            sizes: "any",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
   ],
   server: {
     port: 3000,
-    open: true
+    open: true,
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
+    outDir: "dist",
+    assetsDir: "assets",
     emptyOutDir: true,
-    sourcemap: true
+    sourcemap: true,
   },
-  base:"/",
-})
+  base: "/",
+});
