dc566d06270a856378c23123532bfad8df415854
marpuri ganesh <marpuriganesh@gmail.com>
2024-04-06 13:54:16 +0530

    feat(PWA):

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {VitePWA} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // devOptions: {
+      //   enabled: true
+      // },
+      includeAssets: ['reach_out_hand_logo.svg'],
+      manifest: {
+        name: 'Reach Out',
+        short_name: 'Reach Out',
+        description: 'Reach Out is a platform for people to connect with each other.',
+        theme_color: '#33cfff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'reach_out_hand_logo.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any'
+          }
+        ]}
+    })
+  ],
   server: {
     port: 3000,
     open: true
